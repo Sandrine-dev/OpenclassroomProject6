231a4f00782fbf2a7f422c91c9abf86529bdf3c6
Sandrine-dev <sandrine.volpi@gmail.com>
2020-09-03 10:15:00 +0200

    ajout des models

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://Sandrine:OpenC6@cluster0.newuy.gcp.mongodb.net/SoPekocko?retryWrites=true&w=majority',
+   {useNewUrlParser: true,
+   useUnifiedTopology: true })
+   .then(() => console.log('connexion à MongoDB réussie !'))
+   .catch(() => console.log('Connexion à MongoDB échoué !'));
 
 const app = express();
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+});
+
+app.use(bodyParser.json());
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
