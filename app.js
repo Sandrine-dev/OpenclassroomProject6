a7b2a673391ef9d12a684e5a62d7c9b1310a9c36
Sandrine-dev <sandrine.volpi@gmail.com>
2020-09-03 09:12:22 +0200

    ajout du premier Post

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+});
 
+app.use(bodyParser.json());
+
+app.post('api/sauces',(req, res, next) => {
+   console.log(req.body);
+});
 
 app.use('/api/sauces', (req, res, next) => {
    res.status(200).json(stuff);
