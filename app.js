f3974366df1216ecc04c16cf30b5f1a41133f9c2
Sandrine-dev <sandrine.volpi@gmail.com>
2020-09-03 09:35:41 +0200

    retour en arrière local 4200 non fonctionelle

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-   next();
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
-app.use(bodyParser.json());
+module.exports = app;
 
-app.post('api/sauces',(req, res, next) => {
-   console.log(req.body);
-});
-
-app.use('/api/sauces', (req, res, next) => {
-   res.status(200).json(stuff);
-});
 
 module.exports = app;
